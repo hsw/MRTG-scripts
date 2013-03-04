7c5f086624eaaec1b1bd2fd88b33c3228f34d39a
Antonio <antoniok.spb@gmail.com>
2013-03-04 10:35:22 +0400

    Update mrtg_la.sh
    Misspelling fix.

--- mrtg_la.sh
+++ mrtg_la.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-LC_ALL=C uptime | perl -ne '@F = /^\s*\S+\s+up\s+([^,]+,[^,]+),.+load averages: ([0-9.]+), ([0-9.]+), ([0-9.]+)/; print $F[2]*100,"\n",$F[1]*100,"\n",$F[0],"\nduo.voffka.com load averange\n";'
+LC_ALL=C uptime | perl -ne '@F = /^\s*\S+\s+up\s+([^,]+,[^,]+),.+load averages: ([0-9.]+), ([0-9.]+), ([0-9.]+)/; print $F[2]*100,"\n",$F[1]*100,"\n",$F[0],"\nduo.voffka.com load average\n";'
